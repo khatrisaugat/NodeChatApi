1abe54d6f2386745053adfa6958afe774bc4ba2e
khatrisaugat <khatri.saugat55@gmail.com>
2022-10-31 22:04:40 +0545

    started to implement socket.io

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { createServer } = require("http");
 const app = express();
 const port = process.env.PORT || 3000;
-
+const { Server } = require("socket.io");
 // connection configurations
 // const mySqlConnection = require("./app/helpers/db");
 const db = require("./app/helpers/db");
@@ -15,10 +16,29 @@ db.authenticate()
     console.error("Unable to connect to the database:", err);
   });
 
-app.listen(port, () => console.log("express server running on port 3000.."));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+// socket.io
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => console.log("express server running on port 3000.."));
 
 var routes = require("./app/routes/appRoutes"); //importing route
 routes(app); //register the route
